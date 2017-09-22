90659bce38b9f790a62ddb6a95cf54f981990c1d
Stephen Szpak <srszpak@gmail.com>
2017-09-22 14:59:43 -0500

    initial commit

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.modal').modal();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
